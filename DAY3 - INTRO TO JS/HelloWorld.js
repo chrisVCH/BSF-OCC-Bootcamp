b00068e72000659454cf38d3ff984192f28c7ec1
chrischen <chrischen.mc2@gmail.com>
2024-02-23 09:18:26 -0800

    Added DAY3 - INTRO TO JS

---
+++ DAY3 - INTRO TO JS/HelloWorld.js
@@ -0,0 +1,11 @@
+// this is my first program
+console.log('Hello World!');
+/* console.log('Hello World!');
+console.log('Hello World!'); */
+console.log('Hello World!');
+
+const Name = "Ron";
+
+if (true) {
+    console.log(name);
+}
---
+++ DAY3 - INTRO TO JS/activity.js
@@ -0,0 +1,10 @@
+let Shirt = 17;
+let Shoes = 65;
+let Pants = 49;
+let Sunglasses = 13;
+
+let cart = Shirt*2 + Shoes + Pants + 3*Sunglasses;
+cart *= 0.9;
+
+console.log('Total: $',cart);
+
